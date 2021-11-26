7c1fe42f54c3d2fb763210af5495066e14c1564c
guilhermelima18 <guilhermelima18@hotmail.com>
2021-11-26 01:07:15 -0300

    Construindo a página de post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable prettier/prettier */
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import Prismic from '@prismicio/client';
 
 import { AiOutlineCalendar, AiOutlineUser } from 'react-icons/ai';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -26,45 +34,66 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps) {
+  console.log(postsPagination);
+
   return (
     <section className={styles.sectionPosts}>
-      <Link href="/" passHref>
-        <div className={styles.postCard}>
-          <h2>Como utilizar Hooks</h2>
-          <h4>Pensando em sincronização em vez de ciclos de vida.</h4>
-          <div className={styles.createdAt}>
-            <span>
-              <AiOutlineCalendar fontSize="18" /> <p>15 Mar 2021</p>
-            </span>
-            <span>
-              <AiOutlineUser fontSize="18" /> <p>Joseph de Oliveira</p>
-            </span>
+      {postsPagination.results.map(post => (
+        <Link key={post.uid} href={`post/${post.uid}`} passHref>
+          <div className={styles.postCard}>
+            <h2>{post.data.title}</h2>
+            <h4>{post.data.subtitle}</h4>
+            <div className={styles.createdAt}>
+              <span>
+                <AiOutlineCalendar fontSize="18" />{' '}
+                <p>{post.first_publication_date}</p>
+              </span>
+              <span>
+                <AiOutlineUser fontSize="18" /> <p>{post.data.author}</p>
+              </span>
+            </div>
           </div>
-        </div>
-      </Link>
-
-      <Link href="/" passHref>
-        <div className={styles.postCard}>
-          <h2>Como utilizar Hooks</h2>
-          <h4>Pensando em sincronização em vez de ciclos de vida.</h4>
-          <div className={styles.createdAt}>
-            <span>
-              <AiOutlineCalendar fontSize="18" /> <p>15 Mar 2021</p>
-            </span>
-            <span>
-              <AiOutlineUser fontSize="18" /> <p>Joseph de Oliveira</p>
-            </span>
-          </div>
-        </div>
-      </Link>
+        </Link>
+      ))}
     </section>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 5,
+    }
+  );
 
-//   // TODO
-// };
+  /* console.log(JSON.stringify(postsResponse, null, 2)); */
+
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: format(new Date(), 'PP', {
+        locale: ptBR,
+      }),
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -5,6 +5,17 @@ import { getPrismicClient } from '../../services/prismic';
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
+import { RichText } from 'prismic-dom';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import {
+  AiOutlineUser,
+  AiOutlineCalendar,
+  AiOutlineClockCircle,
+} from 'react-icons/ai';
+
 interface Post {
   first_publication_date: string | null;
   data: {
@@ -26,20 +37,85 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post(): JSX.Element {
-// todo
-// }
+export default function Post({ post }: PostProps): JSX.Element {
+  console.log(post);
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+  return (
+    <>
+      <main className={styles.imgPost}>
+        <img src={post.data.banner.url} alt="Banner Post" />
+      </main>
 
-//   // TODO
-// };
+      <section className={styles.postContent}>
+        <div className={styles.postHeader}>
+          <h1>{post.data.title}</h1>
+          <div className={styles.descriptions}>
+            <span>
+              <AiOutlineCalendar fontSize="18" />
+              <p>{post.first_publication_date}</p>
+            </span>
+            <span>
+              <AiOutlineUser fontSize="18" />
+              <p>{post.data.author}</p>
+            </span>
+            <span>
+              <AiOutlineClockCircle fontSize="18" />
+              <p>4 min</p>
+            </span>
+          </div>
+        </div>
+        <div className={styles.post}>
+          <h3>Proin et varius</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ac nibh
+            non mauris tincidunt vestibulum ut nec dolor. Sed nec sapien vel
+            dolor euismod pharetra. Proin hendrerit massa non tincidunt aliquam.
+            Aliquam lobortis purus lorem, eget tincidunt diam sollicitudin at.
+            Donec cursus nunc sit amet dolor dapibus imperdiet. Quisque blandit
+            libero sed massa mollis, quis tempor tortor dapibus. Vivamus
+            efficitur urna eu lacinia mollis. Vivamus semper dolor in purus
+            elementum posuere. Aliquam orci nisl, feugiat sed finibus non,
+            ultrices id nisl. Morbi vel odio sit amet risus facilisis elementum.
+            Nam sit amet convallis libero. Ut sed convallis arcu. Pellentesque
+            gravida accumsan mi vitae efficitur. Phasellus vitae est nec tellus
+            vehicula tincidunt. Cras eget volutpat tellus, sit amet blandit
+            sapien.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  /* const prismic = getPrismicClient();
+  const posts = await prismic.query(TODO); */
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    first_publication_date: format(new Date(), 'PP', { locale: ptBR }),
+    data: {
+      title: response.data.title,
+      banner: response.data.banner.url,
+      author: response.data.author,
+      content: {
+        heading: response.data.content[0].heading,
+        body: RichText.asHtml(response.data.content[0].body),
+      },
+    },
+  };
+
+  return {
+    props: { post },
+  };
+};
